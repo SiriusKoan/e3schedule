21e6a19eb8edc95cae15a347a375049271024368
SiriusKoan <k.peihsun@gmail.com>
2022-06-25 13:26:46 +0000

    Add link

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
     collector.Visit(url)
 
     // in progress
-    var in_progress [4][]string
+    var in_progress [5][]string
     collector.OnHTML("#news-view-basic-in-progress tbody tr .instancename", func(e *colly.HTMLElement) {
         // HW name
         in_progress[0] = append(in_progress[0], strings.Trim(e.Text, " \t"))
@@ -55,8 +55,12 @@ func main() {
         // people count
         in_progress[3] = append(in_progress[3], strings.Trim(e.Text, " \t"))
     })
-    var submitted [4][]string
-    var overdue [4][]string
+    collector.OnHTML("#news-view-basic-in-progress tbody tr .aalink", func(e *colly.HTMLElement) {
+        // link
+        in_progress[4] = append(in_progress[4], e.Attr("href"))
+    })
+    var submitted [5][]string
+    var overdue [5][]string
     if !only_in_progress{
         // submitted
         collector.OnHTML("#news-view-nofile2-tobegraded-in-progress tbody tr .instancename", func(e *colly.HTMLElement) {
@@ -75,6 +79,10 @@ func main() {
             // people count
             submitted[3] = append(submitted[3], strings.Trim(e.Text, " \t"))
         })
+        collector.OnHTML("#news-view-nofile2-tobegraded-in-progress tbody tr .aalink", func(e *colly.HTMLElement) {
+            // link
+            submitted[4] = append(submitted[4], e.Attr("href"))
+        })
         // overdue
         collector.OnHTML("#news-view-nofile2-notsubmitted-in-progress tbody tr .instancename", func(e *colly.HTMLElement) {
             // HW name
@@ -92,6 +100,10 @@ func main() {
             // people count
             overdue[3] = append(overdue[3], strings.Trim(e.Text, " \t"))
         })
+        collector.OnHTML("#news-view-nofile2-notsubmitted-in-progress tbody tr .aalink", func(e *colly.HTMLElement) {
+            // link
+            overdue[4] = append(overdue[4], e.Attr("href"))
+        })
     }
     // go visiting
     for _, id := range ids {
@@ -103,7 +115,7 @@ func main() {
     t_in_progress.SetOutputMirror(os.Stdout)
     t_in_progress.AppendHeader(table.Row{"Name", "Start", "Due", "Status"})
     for i := 0; i < len(in_progress[0]); i++ {
-        name := in_progress[0][i]
+        name := in_progress[0][i] + "\n" + "\u001b[34m" + in_progress[4][i] + "\u001b[0m"
         start := in_progress[1][i]
         due := in_progress[2][i]
         status := in_progress[3][i]
@@ -112,12 +124,12 @@ func main() {
     t_in_progress.Render()
     if !only_in_progress {
         // submitted
-        fmt.Println("\033[36m[Submitted]\033[0m")
+        fmt.Println("\u001b[32m[Submitted]\033[0m")
         t_submitted := table.NewWriter()
         t_submitted.SetOutputMirror(os.Stdout)
         t_submitted.AppendHeader(table.Row{"Name", "Start", "Due", "Status"})
         for i := 0; i < len(submitted[0]); i++ {
-            name := submitted[0][i]
+            name := submitted[0][i] + "\n" + "\u001b[34m" + submitted[4][i] + "\u001b[0m"
             start := submitted[1][i]
             due := submitted[2][i]
             status := submitted[3][i]
@@ -130,7 +142,7 @@ func main() {
         t_overdue.SetOutputMirror(os.Stdout)
         t_overdue.AppendHeader(table.Row{"Name", "Start", "Due", "Status"})
         for i := 0; i < len(overdue[0]); i++ {
-            name := overdue[0][i]
+            name := overdue[0][i] + "\n" + "\u001b[34m" + overdue[4][i] + "\u001b[0m"
             start := overdue[1][i]
             due := overdue[2][i]
             status := overdue[3][i]
